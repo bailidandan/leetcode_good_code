ca43617cca910cdc1718b8ab1b1a14b688b90747
bailidandan <839356161@qq.com>
2020-05-27 20:46:01 +0800

    Create map.java

---
+++ 工具代码/map.java
@@ -0,0 +1,9 @@
+Map<String, type> map = new HashMap<String, type>();
+	
+	Map<String, Integer> map = new HashMap<String, Integer>();
+	for (int i = 0; i < str.length(); i++) {
+		slope.put(tmp, slope.getOrDefault(type, 0) + 1);//没有就设置为1.
+	}
+  
+  
+  
